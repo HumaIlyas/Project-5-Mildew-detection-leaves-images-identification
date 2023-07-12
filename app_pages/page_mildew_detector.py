7c77403e2b565be678280e49905eaf40c8549bbd
HumaIlyas <>
2023-07-12 15:07:42 +0000

    save model

--- app_pages/page_mildew_detector.py
+++ app_pages/page_mildew_detector.py
@@ -5,10 +5,10 @@ import pandas as pd
 
 from src.data_management import download_dataframe_as_csv
 from src.machine_learning.predictive_analysis import (
-    load_model_and_predict,
-    resize_input_image,
-    plot_predictions_probabilities
-)
+                                                    load_model_and_predict,
+                                                    resize_input_image,
+                                                    plot_predictions_probabilities
+                                                    )
 
 
 def page_mildew_detector_content():
@@ -19,14 +19,14 @@ def page_mildew_detector_content():
     st.info(
         f"* The client is interested in predicting if a cherry leaf is "
         f"healthy or contains powdery mildew."
-        )
+    )
 
     st.write(
         f"* You can download a set of healthy and mildew contained cherry "
         f"leaves for live prediction. "
         f"You can download the images from "
         f"[here](https://www.kaggle.com/ccodeinstitute/cherry-leaves)"
-        )
+    )
 
     st.write("---")
 
@@ -40,11 +40,13 @@ def page_mildew_detector_content():
             img_pil = (Image.open(image))
             st.info(f"Cherry Leaf Sample Image: **{image.name}**")
             img_array = np.array(img_pil)
-            st.image(img_pil, caption=f"Image Size: {img_array.shape[1]}px width x {img_array.shape[0]}px height")
+            st.image(
+                img_pil, caption=f"Image Size: {img_array.shape[1]}px width x {img_array.shape[0]}px height")
 
             version = 'v1'
             resized_img = resize_input_image(img=img_pil, version=version)
-            pred_prob, pred_class = load_model_and_predict(resized_img, version=version)
+            pred_prob, pred_class = load_model_and_predict(
+                resized_img, version=version)
             plot_predictions_probabilities(pred_prob, pred_class)
 
             df_report = df_report.append({"Name": image.name, 'Result': pred_class},
@@ -53,4 +55,5 @@ def page_mildew_detector_content():
         if not df_report.empty:
             st.success("Analysis Report")
             st.table(df_report)
-            st.markdown(download_dataframe_as_csv(df_report), unsafe_allow_html=True)
+            st.markdown(download_dataframe_as_csv(
+                df_report), unsafe_allow_html=True)
