82ed170f91fa737daac3a2a29e8f568390ae7cd4
HumaIlyas <>
2023-07-12 21:50:48 +0000

    Update predictive_analysis.py

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 from tensorflow.keras.models import load_model
+import PIL
 from PIL import Image
 from src.data_management import load_pkl_file
 
@@ -38,7 +39,8 @@ def resize_input_image(img, version):
     Reshape image to average image size
     """
     image_shape = load_pkl_file(file_path=f"outputs/{version}/image_shape.pkl")
-    img_resized = img.resize((image_shape[1], image_shape[0]), Image.ANTIALIAS)
+    img_resized = img.resize(
+        (image_shape[1], image_shape[0]), PIL.Image.Resampling.LANCZOS)
     new_image = np.expand_dims(img_resized, axis=0)/255
 
     return new_image
